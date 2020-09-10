dbe61396f5a2a80cd5f2a1066a9da8446936e0ef
devJang <devjang2016@gmail.com>
2020-09-10 23:35:29 +0900

    feat: chapter-07.08 Remove Middle Man

---
+++ chapter-07/src/08-remove-middle-man/Department.js
@@ -0,0 +1,17 @@
+class Department {
+  get chargeCode() {
+    return this._chargeCode;
+  }
+
+  set chargeCode(arg) {
+    this._chargeCode = arg;
+  }
+
+  get manager() {
+    return this._manager;
+  }
+
+  set manager(arg) {
+    this._manager = arg;
+  }
+}
---
+++ chapter-07/src/08-remove-middle-man/Person.js
@@ -0,0 +1,17 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get department() {
+    return this._department;
+  }
+
+  set department(arg) {
+    this._department = arg;
+  }
+}
